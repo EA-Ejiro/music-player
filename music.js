c3a447ad006f880aa4fcf3d9df1c711e135b0e9e
Ejiro Erharhire <ejiroaustine@gmail.com>
2024-03-30 22:52:35 +0100

    first commit

---
+++ music.js
@@ -0,0 +1,26 @@
+let trackList = [
+    {
+        name: "picture perfect",
+        path: "Wizkid-Picture-Perfect-[TrendyBeatz.com].mp3",
+        img: "picture perfect.jpg",
+        singer: "wizkid",
+    },
+    {
+        name: "pistol",
+        path: "cozycoffeehouse.mp3",
+        img: "anarchy- pistols.jpg",
+        singer: "anarchy",
+    },
+    {
+        name: "mixtape",
+        path: "onrepeat.mp3",
+        img: "linda rodriguez - mixtape.png",
+        singer: "linda rodriguez",
+    },
+    {
+        name: "here comes trouble",
+        path: "rainyday.mp3",
+        img: "dr mic - here comes trouble.jpg",
+        singer: "dr mic",
+    }
+]
